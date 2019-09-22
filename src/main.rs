f7845b5ce059475a4ee3e7799157a5378d611f77
Joe Kim <joe.kim509@gmail.com>
2019-09-22 00:21:50 -0700

    created Chip8 object and constructor initializing all values to 0

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
-
-
 // Define Chip8 sizes
-static MEM_LIMIT: u16 = 4096;     // 4096 byte memory limit
-static DATA_REGISTERS: u8 = 16;   // 16 8 bit data registers, V0 - VF
-static ADDRESS_REGISTER: u16 = 1; // One 16 bit wide register
-static STACK_SIZE: u8 = 48;       // 48 bytes
+pub const MEM_LIMIT: usize = 4096;     // 4096 byte memory limit
+pub const DATA_REGISTERS: usize = 16;   // 16 8 bit data registers, V0 - VF
+pub const ADDRESS_REGISTER: u16 = 1; // One 16 bit wide register
+pub const STACK_SIZE: usize = 48;       // 48 bytes
+pub const SCREEN_WIDTH: usize = 64;  // 64 pixels wide
+pub const SCREEN_HEIGHT: usize = 32; // 32 pixels wide
+pub const KEYPAD_KEYS: usize = 16;   // 16 keys available on the keypad
 
 
 fn main() {
-
-    let mut op_code: u16 = 0;     // This stores the current opcode
-    
     println!("Hello, world!");
+}
+
+struct Chip8 {
+    op_code: u16,
+    memory: [u8; MEM_LIMIT],
+    cpu_register: [u8; DATA_REGISTERS],
+    address_register: u16,
+    program_counter: u16,
+    stack: [u8; STACK_SIZE],
+    gfx: [u8; SCREEN_WIDTH * SCREEN_HEIGHT],
+    delay_timer: u8,
+    sound_timer: u8,
+    key: [u8; KEYPAD_KEYS],
+}
+
+impl Chip8 {
+    pub fn new(&self) -> Chip8 {
+        Chip8 {
+            op_code: 0,
+            memory: [0; MEM_LIMIT],
+            cpu_register: [0; DATA_REGISTERS],
+            address_register: 0,
+            program_counter: 0,
+            stack: [0; STACK_SIZE],
+            gfx: [0; SCREEN_WIDTH * SCREEN_HEIGHT],
+            delay_timer: 0,
+            sound_timer: 0,
+            key: [0; KEYPAD_KEYS],
+        }
+    }
 }
