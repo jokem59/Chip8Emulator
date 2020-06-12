26c1d88e16811d7e2d2b43170e52ff4cc40027c0
Joe Kim <joe.kim509@gmail.com>
2020-06-11 23:01:51 -0700

    structuring to allow for same project in multiple languages

---
+++ rust/target/debug/Chip8Emulator.d
@@ -0,0 +1,1 @@
+c:\Projects\Chip8Emulator\target\debug\Chip8Emulator.pdb: c:\Projects\Chip8Emulator\src\font_set.rs c:\Projects\Chip8Emulator\src\main.rs
---
+++ rust/target/debug/deps/Chip8Emulator-f154c19e0525e087.d
@@ -0,0 +1,6 @@
+c:\Projects\Chip8Emulator\target\debug\deps\Chip8Emulator-f154c19e0525e087.exe: src\main.rs src\font_set.rs
+
+c:\Projects\Chip8Emulator\target\debug\deps\Chip8Emulator-f154c19e0525e087.d: src\main.rs src\font_set.rs
+
+src\main.rs:
+src\font_set.rs:
